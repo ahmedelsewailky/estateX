a554ac477e3d9dc5990b83dccdc31eec38cf0d57
ahmedelsewailky <ahmed.elsewailky@gmail.com>
2024-05-03 22:31:45 +0300

    Completing statistics section with counting

--- js/main.js
+++ js/main.js
@@ -1,9 +1,14 @@
 $(function() {
     "use strict";
 
-    $(window).scroll(function(e) {
-        if ($(this).scrollTop() > 450 && $(this).scrollTop() < 1100) {
-            $(".section.about-us .position-relative").animateTransform("rotate(180deg)")
-        }
+    $(".section.about-us").on("mousemove", function(e) {
+        $(".section.about-us .position-relative").css({
+            transform: `translateX(${e.clientX / 50}px) translateY(${e.clientY / 50}px)`
+        })
+    });
+
+    $('.counter').counterUp({
+        delay: 10,
+        time: 1500
     });
 });
