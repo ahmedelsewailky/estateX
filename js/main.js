989767d7e031f0b163c3d57d2e51e04670a97cc8
ahmedelsewailky <ahmed.elsewailky@gmail.com>
2024-05-06 10:35:51 +0300

    Services, Featured and Sketch Plane sections complete

--- js/main.js
+++ js/main.js
@@ -1,8 +1,8 @@
-$(function() {
+$(function () {
     "use strict";
 
-    $(".section.about-us").on("mousemove", function(e) {
-        $(".section.about-us .position-relative").css({
+    $("section.about-us").on("mousemove", function (e) {
+        $("section.about-us .position-relative").css({
             transform: `translateX(${e.clientX / 50}px) translateY(${e.clientY / 50}px)`
         })
     });
@@ -11,4 +11,30 @@ $(function() {
         delay: 10,
         time: 1500
     });
+
+    $('.owl-carousel').owlCarousel({
+        loop: true,
+        margin: 10,
+        responsiveClass: true,
+        autoplay: true,
+        dots: true,
+        navText: [
+            "<i class='bx bx-left-arrow-alt' ></i>", "<i class='bx bx-right-arrow-alt' ></i>"
+        ],
+        responsive: {
+            0: {
+                items: 1,
+                nav: true
+            },
+            600: {
+                items: 3,
+                nav: false
+            },
+            1000: {
+                items: 3,
+                nav: true,
+                loop: true
+            }
+        }
+    })
 });
